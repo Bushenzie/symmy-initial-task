0eb7575e7eebb4c36e6bd5ffe3db985f45afa2b4
Daniel Buchal <daniel.buchal01@gmail.com>
2025-04-17 16:31:11 +0200

    feat: Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return <></>;
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <button
+        className="py-2 px-6 cursor-pointer text-white bg-blue-600 hover:bg-blue-400 transition-all duration-200 rounded-2xl"
+        onClick={handleClick}
+      >
+        Count: {count}
+      </button>
+    </div>
+  );
 }
 
 export default App;
