a872c926d551075a51ae99ecfccd5bef2d48dbb4
Daniel Buchal <daniel.buchal01@gmail.com>
2025-04-17 16:26:45 +0200

    feat: Minimal ESLint & Prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,87 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import reactA11y from "eslint-plugin-jsx-a11y";
+import prettier from "eslint-config-prettier/flat";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier,
+    ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "jsx-a11y": reactA11y,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      react: react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-use-before-define": "error",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/no-useless-empty-export": "error",
+      "@typescript-eslint/method-signature-style": "error",
+
+      "no-console": [
+        "warn",
+        {
+          allow: ["warn", "error"],
+        },
+      ],
+      "no-alert": "warn",
+      "no-debugger": "warn",
+      "no-promise-executor-return": "error",
+      "no-unreachable-loop": "error",
+      "no-mixed-spaces-and-tabs": "error",
+      "no-implicit-coercion": "error",
+      "no-invalid-this": "error",
+      "no-loop-func": "error",
+      "no-multi-str": "error",
+      "no-new": "error",
+      "no-return-assign": "error",
+      "no-unneeded-ternary": "error",
+      "no-unused-expressions": "error",
+      "no-useless-computed-key": "error",
+      "no-useless-concat": "error",
+      "no-useless-rename": "error",
+      "no-useless-return": "error",
+      "no-var": "error",
+      "multiline-comment-style": "off",
+      "object-shorthand": "error",
+      eqeqeq: "error",
+      "prefer-const": "error",
+      "prefer-destructuring": "error",
+      "prefer-exponentiation-operator": "error",
+      "prefer-object-spread": "error",
+      "prefer-promise-reject-errors": "error",
+      "prefer-regex-literals": "error",
+      "prefer-rest-params": "error",
+      "prefer-spread": "error",
+      "prefer-template": "error",
+      "prefer-arrow-callback": "error",
     },
-  },
-)
+  }
+);
