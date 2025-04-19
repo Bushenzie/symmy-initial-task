188c2c65566d91086c6d9a475bb267f7ae7a302d
Daniel Buchal <daniel.buchal01@gmail.com>
2025-04-19 11:04:01 +0200

    feat: Added query params to store page num

--- src/components/organisms/ProductList.tsx
+++ src/components/organisms/ProductList.tsx
@@ -1,8 +1,8 @@
+import { useNavigate, useSearch } from "@tanstack/react-router";
 import { Redo2, Undo2 } from "lucide-react";
 import { useState } from "react";
 import { useGetProducts } from "../../api/useGetProducts";
 import { Button } from "../atoms/Button";
-import LoadingSpinner from "../atoms/LoadingSpinner";
 import { ProductCard } from "../molecules/ProductCard";
 
 /**
@@ -12,47 +12,45 @@ import { ProductCard } from "../molecules/ProductCard";
  */
 
 export const ProductList: React.FC = () => {
+  const navigate = useNavigate({ from: "/" });
+  const { page } = useSearch({
+    from: "/",
+  });
+
   const productsPerPage = 20;
-  const [currentPage, setCurrentPage] = useState(1);
-  const { data, isLoading, isError, isSuccess } = useGetProducts({
+
+  const [currentPage, setCurrentPage] = useState(page);
+  const { data } = useGetProducts({
     page: currentPage,
     limit: productsPerPage,
   });
 
+  // TODO: this should be done in more elegant way
   const changePage = (by: number) => {
+    navigate({ search: (prev) => ({ page: prev.page + by }) });
     setCurrentPage((prev) => prev + by);
   };
 
   return (
     <div className="h-screen">
-      {isLoading && <LoadingSpinner />}
-      {isError && <h2 className="text-red-500">Something went wrong</h2>}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 items-center gap-4">
-        {isSuccess && (
-          <>
-            {data.map((product) => (
-              <ProductCard product={product} key={product.id} />
-            ))}
-            {data.length === 0 && (
-              <h2 className=" text-gray-400 col-span-full text-center">
-                There are no products here
-              </h2>
-            )}
-            <div className="col-span-full flex items-center justify-center gap-2 mb-8">
-              <Button onClick={() => changePage(-1)} isDisabled={currentPage === 1}>
-                <Undo2 size={14} />
-                Prev. page
-              </Button>
-              <Button
-                onClick={() => changePage(1)}
-                isDisabled={data.length === 0 || data.length < productsPerPage}
-              >
-                Next page
-                <Redo2 size={14} />
-              </Button>
-            </div>
-          </>
+        {data?.length === 0 && (
+          <h2 className=" text-gray-400 col-span-full text-center">There are no products here</h2>
         )}
+        {data?.map((product) => <ProductCard product={product} key={product.id} />)}
+        <div className="col-span-full flex items-center justify-center gap-2 mb-8">
+          <Button onClick={() => changePage(-1)} isDisabled={currentPage === 1}>
+            <Undo2 size={14} />
+            Prev. page
+          </Button>
+          <Button
+            onClick={() => changePage(1)}
+            isDisabled={data.length === 0 || data.length < productsPerPage}
+          >
+            Next page
+            <Redo2 size={14} />
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
 import ProductsPage from "../components/pages/ProductsPage";
 
+type QueryParams = {
+  page: number;
+};
+
 export const Route = createFileRoute("/")({
+  validateSearch: (searchParams: Record<string, string>): QueryParams => {
+    const pageNum = Number(searchParams?.page);
+
+    return {
+      page: !isNaN(pageNum) ? Math.abs(pageNum) : 1,
+    };
+  },
   component: ProductsPage,
 });
